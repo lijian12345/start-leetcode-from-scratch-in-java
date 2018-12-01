250e4de4bfb1be610f30cc07101cefc1c0a72a7b
lijian12345 <1053294286@qq.com>
2018-12-01 14:11:26 +0800

    1. Two Sum(Java)

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] temp = new int[2];
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    temp[0] = i;
+                    temp[1] = j;
+                    break;
+                }
+            }
+        }
+        return temp;
+    }
+}
