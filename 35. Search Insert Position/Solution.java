36887f1d1147dd1e92eb6ffe4893b414b9d59650
lijian12345 <1053294286@qq.com>
2018-12-10 16:07:16 +0800

    78.00%

--- 35. Search Insert Position/Solution.java
+++ 35. Search Insert Position/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int index = 0;
         int len = nums.length;
-        // 下句中len直接用nums.length的话会慢很多
+        // 下句中len直接用nums.length的话会慢很多（存疑）
         while (index < len && nums[index] < target) {
             index++;
         }
