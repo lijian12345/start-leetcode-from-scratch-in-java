ff40b03fa09bdba863f31d953562792b09fd140d
lijian12345 <1053294286@qq.com>
2018-12-19 19:41:35 +0800

    6.37% in the case of using comparator and hashmap

---
+++ Array/Easy/169. Majority Element/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    private class ValueComparator implements Comparator<Map.Entry<Integer, Integer>> {
+        public int compare(Map.Entry<Integer, Integer> m1, Map.Entry<Integer, Integer> m2) {
+            return m2.getValue() - m1.getValue();
+        }
+    }
+
+    public int majorityElement(int[] nums) {
+        // int min = nums.length / 2;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (m.containsKey(nums[i])) {
+                m.put(nums[i], m.get(nums[i]) + 1);
+            } else {
+                m.put(nums[i], 1);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>();
+        list.addAll(m.entrySet());
+        Solution.ValueComparator vc = new ValueComparator();
+        Collections.sort(list, vc);
+        return list.get(0).getKey();
+    }
+}
