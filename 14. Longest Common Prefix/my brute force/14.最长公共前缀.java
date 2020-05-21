0f6d3cf72a08a7133ad1fbe6f76ccb4209a3e94a
lijian12345 <1053294286@qq.com>
2020-05-21 23:37:42 +0800

    completed the 14th probelm

---
+++ 14. Longest Common Prefix/my brute force/14.最长公共前缀.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        StringBuilder res = new StringBuilder();
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (str.length() < minLen) {
+                minLen = str.length();
+            }
+        }
+        for (int i = 0; i < minLen; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != c) {
+                    return res.toString();
+                }
+            }
+            res.append(c);
+        }
+        return res.toString();
+    }
+}
+// @lc code=end
+
