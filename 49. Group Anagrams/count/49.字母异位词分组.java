4fc0578e42c493ed5adb71422ce73212978b2f37
lijian12345 <1053294286@qq.com>
2020-06-26 01:50:05 +0800

    complete the 49th problem

---
+++ 49. Group Anagrams/count/49.字母异位词分组.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> res = new HashMap<>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+// @lc code=end
+
---
+++ 49. Group Anagrams/sort/49.字母异位词分组.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+// @lc code=end
+
