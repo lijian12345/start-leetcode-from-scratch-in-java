0d4b7e885ff387a411b6ef4eacb411db926820da
lijian12345 <1053294286@qq.com>
2020-07-22 23:38:52 +0800

    complete the 62nd problem

---
+++ 62. Unique Paths/dp(n)/62.不同路径.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+    // 杨辉三角
+    public int uniquePaths(int m, int n) {
+        int[] cur = new int[n];
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+}
+// @lc code=end
+
