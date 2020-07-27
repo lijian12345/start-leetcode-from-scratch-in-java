fd384492dce35e031d98d8dfbd57525974e088ab
lijian12345 <1053294286@qq.com>
2020-07-27 17:57:22 +0800

    complete the 88th problem

---
+++ 88. Merge Sorted Array/88.合并两个有序数组.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=java
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1;
+        int p = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[p--] = nums1[i--];
+            } else {
+                nums1[p--] = nums2[j--];
+            }
+        }
+        while (i >= 0) {
+            nums1[p--] = nums1[i--];
+        }
+        while (j >= 0) {
+            nums1[p--] = nums2[j--];
+        }
+    }
+}
+// @lc code=end
+
