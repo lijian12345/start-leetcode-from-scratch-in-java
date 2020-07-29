b3059bb1568cc7eea691b17e0240a521e7ba5d59
lijian12345 <1053294286@qq.com>
2020-07-29 15:40:36 +0800

    complete the 104th problem

---
+++ 104. Maximum Depth of Binary Tree/queue/104.二叉树的最大深度.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=java
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            res++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
