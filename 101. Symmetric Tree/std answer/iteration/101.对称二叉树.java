7c5d940fa492a98a1890be1d8f993647a6d2e3ca
lijian12345 <1053294286@qq.com>
2020-07-29 15:27:48 +0800

    complete the 102nd problem

---
+++ 101. Symmetric Tree/std answer/iteration/101.对称二叉树.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+
+    private boolean isSymmetric(TreeNode u, TreeNode v) {
+        // LinkedList可以添加null，ArrayDeque不行
+        Deque<TreeNode> q = new LinkedList<>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
+    }
+}
+// @lc code=end
+
