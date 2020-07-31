2bcb014d40533b0e5c149e184f366befea1da8d4
lijian12345 <1053294286@qq.com>
2020-07-31 17:26:14 +0800

    complete the 123rd problem

---
+++ 123. Best Time to Buy and Sell Stock III/std answer/123.买卖股票的最佳时机-iii.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=java
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1)
+            return 0;
+        int max_k = 2;
+        int n = prices.length;
+        int[][][] dp = new int[n][max_k + 1][2];
+        for (int i = 0; i < n; i++) {
+            for (int k = max_k; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[0];
+                    continue;
+                }
+                dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        }
+        // 穷举了 n × max_k × 2 个状态，正确。
+        return dp[n - 1][max_k][0];
+    }
+}
+// @lc code=end
+
