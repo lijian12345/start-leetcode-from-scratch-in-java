590481c4179282087d1a6dc8496455e830fde86b
lijian12345 <1053294286@qq.com>
2020-07-31 16:08:10 +0800

    complete the 121st problem

---
+++ 121. Best Time to Buy and Sell Stock/std answer/121.买卖股票的最佳时机.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
+// @lc code=end
+
