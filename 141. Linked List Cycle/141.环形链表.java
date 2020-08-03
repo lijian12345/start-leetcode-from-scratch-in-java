c3a6c9de48e7709b2e4f10de40ae623a90f6723d
lijian12345 <1053294286@qq.com>
2020-08-04 01:57:29 +0800

    complete the 141st problem

---
+++ 141. Linked List Cycle/141.环形链表.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slowPointer = head, fastPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
