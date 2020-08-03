507332b6b48c14ccda782960d44e1fb0a76c7320
lijian12345 <1053294286@qq.com>
2020-08-03 22:07:37 +0800

    complete the 136th problem

---
+++ 136. Single Number/136.只出现一次的数字.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=java
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+// @lc code=end
+
