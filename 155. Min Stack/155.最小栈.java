bbf5deff30c17f063f3a049037e305ed158e140b
lijian12345 <1053294286@qq.com>
2020-08-04 23:00:19 +0800

    complete the 155th problem

---
+++ 155. Min Stack/155.最小栈.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=java
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+class MinStack {
+    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> minStack = new ArrayDeque<>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
+
