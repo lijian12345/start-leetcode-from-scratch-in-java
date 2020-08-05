dc9b7f48e3cc68bf73f2cdd7f10bf6c41863c7b7
lijian12345 <1053294286@qq.com>
2020-08-05 21:12:13 +0800

    complete the 188th problem

---
+++ 188. Best Time to Buy and Sell Stock IV/188.买卖股票的最佳时机-iv.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=188 lang=java
+ *
+ * [188] 买卖股票的最佳时机 IV
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (k > n / 2) {
+            return maxProfit_k_inf(prices);
+        }
+        int[][][] dp = new int[n][k + 1][2];
+        for (int i = 0; i < n; i++) {
+            for (int j = k; j >= 1; j--) {
+                if (i - 1 == -1) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[0];
+                    continue;
+                }
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][k][0];
+    }
+
+    private int maxProfit_k_inf(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+}
+// @lc code=end
+
