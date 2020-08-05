79ff1e0d1c40f545c2b5c5f644c8b62782211464
lijian12345 <1053294286@qq.com>
2020-08-05 15:10:17 +0800

    complete the 168th problem

---
+++ 168. Excel Sheet Column Title/168.excel表列名称.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=java
+ *
+ * [168] Excel表列名称
+ */
+
+// @lc code=start
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int c = n % 26;
+            if (c == 0) {
+                c = 26;
+                n -= 1;
+            }
+            sb.append((char) ('A' + c - 1));
+            n /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
+// @lc code=end
+
