cf25d0d2d5ea729ee57ee4b7d2b01cc9b777c804
lijian12345 <1053294286@qq.com>
2020-08-07 22:35:25 +0800

    complete the 213th problem

---
+++ 213. House Robber II/213.打家劫舍-ii.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        if (nums.length == 1)
+            return nums[0];
+        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length - 1)),
+                myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+
+    private int myRob(int[] nums) {
+        int pre = 0, cur = 0, tmp;
+        for (int num : nums) {
+            tmp = cur;
+            cur = Math.max(pre + num, cur);
+            pre = tmp;
+        }
+        return cur;
+    }
+}
+// @lc code=end
+
