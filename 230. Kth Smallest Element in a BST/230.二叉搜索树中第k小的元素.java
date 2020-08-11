68217d6ab2bb6734fa2a20b71a3b7f6b435df641
lijian12345 <1053294286@qq.com>
2020-08-11 17:39:50 +0800

    complete the 230th problem

---
+++ 230. Kth Smallest Element in a BST/230.二叉搜索树中第k小的元素.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=java
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            k--;
+            if (k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
