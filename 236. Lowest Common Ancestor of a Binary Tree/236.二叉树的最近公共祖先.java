05a31153de375260516e50737e10c9b43ee56899
lijian12345 <1053294286@qq.com>
2020-08-12 16:08:03 +0800

    complete the 236th problem

---
+++ 236. Lowest Common Ancestor of a Binary Tree/236.二叉树的最近公共祖先.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=java
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 如果 p 和 q 都在树 root 中，则返回它们的公共祖先；
+    // 如果树 root 中只存在一个，则返回存在的一个；
+    // 如果 p 和 q 都不存在树 root 中，则返回 null
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null)
+            return right;
+        if (right == null)
+            return left;
+        return root;
+    }
+}
+// @lc code=end
+
