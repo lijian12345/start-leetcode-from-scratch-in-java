7813dfbedbb41e8848ca5a89a2fa3e457f19bdd3
lijian12345 <1053294286@qq.com>
2020-08-29 01:19:40 +0800

    complete the 392nd problem

---
+++ 392. Is Subsequence/two-pointer/392.判断子序列.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=392 lang=java
+ *
+ * [392] 判断子序列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        int m = s.length(), n = t.length();
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == m;
+    }
+}
+// @lc code=end
+
