ef955bb9b1a028bf23eb518de605694f275e9507
lijian12345 <1053294286@qq.com>
2020-09-04 02:42:46 +0800

    complete the 406th problem

---
+++ 406. Queue Reconstruction by Height/406.根据身高重建队列.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=406 lang=java
+ *
+ * [406] 根据身高重建队列
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
+        List<int[]> res = new LinkedList<>();
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[people.length][2]);
+    }
+}
+// @lc code=end
+
