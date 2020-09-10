e5f73b61046e9b909ad859b67f562d27f052ca86
lijian12345 <1053294286@qq.com>
2020-09-11 00:40:31 +0800

    complete the 474th problem

---
+++ 474. Ones and Zeroes/474.一和零.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=474 lang=java
+ *
+ * [474] 一和零
+ */
+
+// @lc code=start
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s : strs) {
+            int[] zeroAndOne = calZeroAndOne(s);
+            int zeros = zeroAndOne[0];
+            int ones = zeroAndOne[1];
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    private int[] calZeroAndOne(String s) {
+        int[] res = new int[2];
+        for (char c : s.toCharArray()) {
+            res[c - '0']++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
