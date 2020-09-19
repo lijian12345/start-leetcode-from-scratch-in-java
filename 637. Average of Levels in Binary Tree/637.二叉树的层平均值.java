fed86832ce2421138797ee9129dcdeb8ea780d5c
lijian12345 <1053294286@qq.com>
2020-09-19 23:50:24 +0800

    complete the 637th problem

---
+++ 637. Average of Levels in Binary Tree/637.二叉树的层平均值.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            double curSum = 0.0;
+            double curNum = queue.size();
+            for (int i = 0; i < curNum; i++) {
+                TreeNode node = queue.poll();
+                curSum += node.val;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(curSum / curNum);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
