66daebbb6ab97e48f391f84e62b089e5457e66d1
lijian12345 <1053294286@qq.com>
2020-09-20 19:56:18 +0800

    complete the 665th problem

---
+++ 665. Non-decreasing Array/665.非递减数列.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=665 lang=java
+ *
+ * [665] 非递减数列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count <= 1; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+        return count <= 1;
+    }
+}
+// @lc code=end
+
