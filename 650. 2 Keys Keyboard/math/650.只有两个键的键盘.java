68ef90ff156d315e603f028bdbc78b1931f9adab
lijian12345 <1053294286@qq.com>
2020-09-20 16:55:56 +0800

    complete the 650th problem

---
+++ 650. 2 Keys Keyboard/math/650.只有两个键的键盘.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=650 lang=java
+ *
+ * [650] 只有两个键的键盘
+ */
+
+// @lc code=start
+class Solution {
+    public int minSteps(int n) {
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                res += i;
+                n /= i;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
