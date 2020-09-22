a43efe6108ac8226aa93992aab0af632e7dff523
lijian12345 <1053294286@qq.com>
2020-09-22 17:20:36 +0800

    complete the 744th problem

---
+++ 744. Find Smallest Letter Greater Than Target/744.寻找比目标字母大的最小字母.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=744 lang=java
+ *
+ * [744] 寻找比目标字母大的最小字母
+ */
+
+// @lc code=start
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return letters[low % letters.length];
+    }
+}
+// @lc code=end
+
