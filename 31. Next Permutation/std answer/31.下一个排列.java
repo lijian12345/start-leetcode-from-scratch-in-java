183446e9fb2edee8a042252f64c4c163ae00fbd4
lijian12345 <1053294286@qq.com>
2020-09-25 22:17:31 +0800

    update the 31st problem

--- 31. Next Permutation/std answer/31.下一个排列.java
+++ 31. Next Permutation/std answer/31.下一个排列.java
@@ -8,32 +8,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums, i, j);
+        if (i < 0) {
+            reverse(nums, 0, nums.length - 1);
+            return;
         }
-        reverse(nums, i + 1);
-    }
-
-    private void reverse(int[] nums, int start) {
-        int i = start, j = nums.length - 1;
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
+        int j = nums.length - 1;
+        while (j >= 0 && nums[j] <= nums[i]) {
             j--;
         }
+        swap(nums, i, j);
+        reverse(nums, i + 1, nums.length - 1);
     }
 
     private void swap(int[] nums, int i, int j) {
         int t = nums[i];
         nums[i] = nums[j];
         nums[j] = t;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
     }
 }
 // @lc code=end
