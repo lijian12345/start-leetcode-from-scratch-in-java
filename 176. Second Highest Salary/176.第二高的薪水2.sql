8b0cc135fab46a7f37888a56c1fcfcbf7bbea602
lijian12345 <1053294286@qq.com>
2020-09-27 23:08:28 +0800

    complete the 176th problem

---
+++ 176. Second Highest Salary/176.第二高的薪水2.sql
@@ -0,0 +1,21 @@
+--
+-- @lc app=leetcode.cn id=176 lang=mysql
+--
+-- [176] 第二高的薪水
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT ifnull(
+        (
+            SELECT DISTINCT Salary
+            FROM Employee
+            ORDER BY Salary DESC
+            LIMIT 1 OFFSET 1
+        ),
+        NULL
+    ) AS SecondHighestSalary
+    
+-- @lc code=end
+
