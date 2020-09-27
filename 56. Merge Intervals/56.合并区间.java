dc51e482ea4cf29a8676900e55cf55edab959d88
lijian12345 <1053294286@qq.com>
2020-09-27 18:56:01 +0800

    complete the 56th problem

---
+++ 56. Merge Intervals/56.合并区间.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[] {L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][2]);
+    }
+}
+// @lc code=end
+
