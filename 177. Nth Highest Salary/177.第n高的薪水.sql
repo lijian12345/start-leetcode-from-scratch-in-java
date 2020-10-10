0eeb212fb132ff4d564e01d85d87c5381fdb3518
lijian12345 <1053294286@qq.com>
2020-10-10 23:07:53 +0800

    complete the 177th problem

---
+++ 177. Nth Highest Salary/177.第n高的薪水.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode.cn id=177 lang=mysql
+--
+-- [177] 第N高的薪水
+--
+-- @lc code=start
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT BEGIN
+SET N := N -1;
+RETURN (
+  # Write your MySQL query statement below.
+  SELECT salary
+  FROM employee
+  GROUP BY salary
+  ORDER BY salary DESC
+  LIMIT N, 1
+);
+END
+END -- @lc code=end
